ef4090fa17b70081bdf6caa2fa4ab9e8f32dde6e
Andrew Gnias <AGnias47@gmail.com>
2017-11-18 19:48:32 +0000

    Upgraded script for python3

--- secret_santa.py
+++ secret_santa.py
@@ -1,83 +1,79 @@
-#!/usr/bin/env python
+#!/usr/bin/python3.4
 #
 #	A. Gnias
 #
 #	secret_santa.py - Informs Secret Santa participants of their Secret Santa
 #	via email, making it blind from the person running the code.
 #
-#	10/01/2016
+#	11/17/2017
 #
-#	Ubuntu Linux 3.4.0+
-#	Python 2.7.6
+#  Linux raspberrypi 4.1.7+
+#	Python 3.4.2
 #
+
 import sys
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
 from random import shuffle
 
 def compose_message(gifter, recipient) :
 	"""Composes body of Secret Santa email"""
-	exchange_date = "December 27, 2016"
- 	return "%s,\n\tYou have been assigned to be %s's Secret Santa! Please \
-purchase a gift for them before the gift exchange on \
-%s."%(gifter,recipient,exchange_date)
+	exchange_date = "December 20, 2017"
+	return ("{}, \n\nYou have been assigned to be {}'s Secret Santa! Please purchase a gift for them before \
+the gift exchange on {}".format(gifter, recipient, exchange_date))
 
-def test_message(gifter, recipient) :
+def test_message() :
 	"""Generic message to test everyone's emails."""
-	return "Hello!\n\tThis is a test of the Secret Santa assignment system.\
+	return "Hello!\n\nThis is a test of the Secret Santa assignment system.\
 To confirm that you are able to receive this email, please let Andy Gnias \
 know that you have received this message by replying directly to this \
 email."
 
 def send_email(from_address,from_password,gifter_email,gifter,recipient) :
 	"""Sends Secret Santa email"""
-	message = MIMEMultipart()
-	server = smtplib.SMTP('smtp.gmail.com',587)
-	server.starttls()
-	server.login(from_address,from_password)
-	message['From'] = from_address
-	message['To'] = gifter_email
-	message['Subject'] = "Secret Santa 2016!"
+	#message_body = test_message()
 	message_body = compose_message(gifter,recipient)
-	message.attach(MIMEText(message_body, 'plain'))
-	text = message.as_string()
-	server.sendmail(from_address, gifter_email, text)
-	server.quit()
+	try:  
+		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+		server.ehlo()
+		server.login(from_address, from_password)
+		server.sendmail(from_address, gifter_email, message_body)
+		server.close()
+		print('Email sent!')
+	except:  
+		print("Error sending email to {}".format(gifter_email))
 
 def check_conditions(nlist) :
 	"""Prevents people who shouldn't get each other from getting each other
 	i.e. couples."""
 	for i in range(len(nlist)) :
-		first = i
-		second = i+1
-		if second == len(nlist) :
-			second = 0
-		if nlist[first] == "Andy" and nlist[second] == "Drew" or \
-		nlist[first] == "Drew" and nlist[second] == "Andy" :
+		recp = i
+		gifter = i+1
+		if gifter == len(nlist) :
+			gifter = 0
+		if nlist[gifter] == "Secret" and nlist[recp] == "Santa" or \
+		nlist[gifter] == "Santa" and nlist[recp] == "Secret" :
 			return False
 	return True
 
 def main() :
 	argc = len(sys.argv)
-
-	from_address = "email@gmail.com"
+	from_address = "________@gmail.com"
 	if argc == 1 :
-		print "Requires %s email password as argument"%from_address
+		print("Requires {} email password as argument".format(from_address))
 		sys.exit()
-
 	from_password = sys.argv[1]
 
-	d = dict()
-#	d["Andy"] = "email@gmail.com"
-#	continue in same form for other patricipants
-	names = d.keys()
+	d = dict() #hash with key=names value=email
+	d["Santa"] = "GimmeDemCookies69420@gmail.com"
+	#continue in same format for other participants
+
+	names = list(d.keys())
 	list_sorted = False
-	while not list_sorted :
+	while not list_sorted : #shuffle list until it abides by conditions set
 		shuffle(names)
 		list_sorted = check_conditions(names)
 
-	print names
+	print(names)
 
 	#first person gifts to last name in 'names list
 	gifter = names[0]

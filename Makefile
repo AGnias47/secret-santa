efcb35a21f34a086f54194590d39936fa1b6d21f
Andy Gnias <AGnias47@gmail.com>
2022-11-14 19:07:34 -0500

    Ready for invitations

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ format:            ## Format all Python code using black
 	black -l 120 .
 
 clean:             ## Clean the directory
-	git clean -dxf
+	git clean -dxf -e pii/ -e venv/
 
 static:            ## Lint Python files using pylint
 	pylint --disable=C0103,C0301,C0411,R1711,R1705,R1734,R1735,R0903 main.py secret_santa/ || true
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-#
-#    Andy Gnias
-#
-#    main.py - Informs Secret Santa participants of their Secret Santa via email
-#
-#    Kubuntu 5.21.4
-#    Python 3.9.7
-#
 
 from secret_santa.Participant import create_participant_hash
 from secret_santa.selections import make_selections
-from secret_santa.email import email_participants
+from secret_santa.email import invite_participants, email_participants
 from send_email.gmail_api import GmailApiSender
 from send_email.aws_ses import AwsSesSender
 from send_email.gmail_smtp import GmailSMTPSender
@@ -19,6 +11,10 @@ import argparse
 from getpass import getpass
 import pathlib
 import re as regex
+
+"""
+Informs Secret Santa participants of their Secret Santa via email
+"""
 
 
 def process_commandline_parameters():
@@ -34,71 +30,108 @@ def process_commandline_parameters():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--email", help="Sender's Email address")
-    parser.add_argument("-p", "--password", help="Sender's Email password; only necessary for SMTP through Gmail")
-    parser.add_argument("-m", "--method", help="Method for sending emails; One of gmailapi (default), ses, or smtp.")
-    parser.add_argument("-n", "--names", help="CSV containing participants names and email addresses")
     parser.add_argument(
-        "-x", "--exceptions", help="CSV containing participant's name followed by people they should not be paired with"
+        "-p",
+        "--password",
+        help="Sender's Email password; only necessary for SMTP through Gmail",
+    )
+    parser.add_argument(
+        "-m",
+        "--method",
+        help="Method for sending emails; One of gmailapi (default), ses, or smtp.",
+    )
+    parser.add_argument(
+        "-n", "--names", help="CSV containing participants names and email addresses"
+    )
+    parser.add_argument(
+        "-x",
+        "--exceptions",
+        help="CSV containing participant's name followed by people they should not be paired with",
     )
     parser.add_argument("-d", "--date", help="Date of the Secret Santa exchange")
+    parser.add_argument(
+        "-i",
+        "--invitation",
+        help="Sends an invitation email instead of pairing participants for the exchange",
+        action="store_true"
+    )
     args = parser.parse_args()
     if args.email:
         email = args.email
     else:
         email = input("Sender's email: ").strip()
     if args.method:
-        if args.method.lower() == "gmailapi":
+        if args.method.casefold() == "gmailapi".casefold():
             sender_object = GmailApiSender(email)
-        elif args.method.lower() == "ses":
+        elif args.method.casefold() == "ses".casefold():
             sender_object = AwsSesSender(email)
-        elif args.method.lower() == "smtp":
+        elif args.method.casefold() == "smtp".casefold():
             if args.password:
                 password = args.password
             else:
                 password = getpass("Password for sender's email: ").strip()
             sender_object = GmailSMTPSender(email, password)
         else:
-            raise ValueError("Invalid send method specified; must be one of gmailapi, ses, or smtp")
+            raise ValueError(
+                "Invalid send method specified; must be one of gmailapi, ses, or smtp"
+            )
     else:
         sender_object = GmailApiSender(email)
     if args.names:
         names_filename = args.names
     else:
-        generic_names_file = pathlib.Path("names.csv")
+        generic_names_file = pathlib.Path("pii/names.csv")
         if generic_names_file.is_file():
-            print("Using names.csv from cwd")
-            names_filename = "names.csv"
+            print("Using pii/names.csv from cwd")
+            names_filename = "pii/names.csv"
         else:
             names_filename = input("File containing names,emails: ").strip()
     if args.exceptions:
         exceptions_filename = args.exceptions
     else:
-        generic_exceptions_file = pathlib.Path("exceptions.csv")
+        generic_exceptions_file = pathlib.Path("pii/exceptions.csv")
         if generic_exceptions_file.is_file():
-            print("Using exceptions.csv from cwd")
-            exceptions_filename = "exceptions.csv"
+            print("Using pii/exceptions.csv from cwd")
+            exceptions_filename = "pii/exceptions.csv"
         else:
-            confirm_exceptions_file = input("No exceptions file provided (-x). Would you like to use one? ").strip()
+            confirm_exceptions_file = input(
+                "No exceptions file provided (-x). Would you like to use one? "
+            ).strip()
             if regex.match("[Yy]", confirm_exceptions_file):
                 exceptions_filename = input("File name: ").strip()
             else:
                 exceptions_filename = None
     if args.date:
         exchange_date_string = args.date
-    else:
-        confirm_exchange_date = input("Would you like to specify an exchange date?").strip()
+    elif not args.invitation:
+        confirm_exchange_date = input(
+            "Would you like to specify an exchange date?"
+        ).strip()
         if regex.match("[Yy]", confirm_exchange_date):
-            exchange_date_string = input("Specify date however you would like integration_test displayed: ").strip()
+            exchange_date_string = input(
+                "Specify date however you would like integration_test displayed: "
+            ).strip()
         else:
             exchange_date_string = None
-    return sender_object, names_filename, exceptions_filename, exchange_date_string
+    else:
+        exchange_date_string = None
+    return sender_object, names_filename, exceptions_filename, exchange_date_string, args.invitation
 
 
 if __name__ == "__main__":
-    sender, names_fname, exceptions_fname, exchange_date = process_commandline_parameters()
+    (
+        sender,
+        names_fname,
+        exceptions_fname,
+        exchange_date,
+        invitation
+    ) = process_commandline_parameters()
     participants = create_participant_hash(names_fname, exceptions_fname)
-    order = make_selections(participants)  # The Algorithm
-    email_status = email_participants(sender, participants, order, exchange_date)
+    if invitation:
+        email_status = invite_participants(sender, participants)
+    else:
+        order = make_selections(participants)  # The Algorithm
+        email_status = email_participants(sender, participants, order, exchange_date)
     if all(email_status):
         print("All emails sent successfully")
     else:
